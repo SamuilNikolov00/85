e15003e3e01d8cf38cd7be1619008b95a04b6b71
Vladislav Kotsev <vladygk8@gmail.com>
2023-01-11 20:01:35 +0200

    Add: functionality

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,23 @@
 import "../scss/app.scss";
 
-window.addEventListener("DOMContentLoaded", () => {
+async function GetData() {
+  const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=10");
+  const data = await response.json();
+  return data.results.map((x) => x.name);
+  
+}
+
+window.addEventListener("DOMContentLoaded",async () => {
   // This block will be executed once the page is loaded and ready
 
   const ul = document.querySelector("ul");
+
+  const pokemonNames = await GetData();
+
+  for(let pokemon of pokemonNames){
+    const li = document.createElement("li");
+    li.innerText = pokemon;
+    ul.appendChild(li);
+  }
+
 });
